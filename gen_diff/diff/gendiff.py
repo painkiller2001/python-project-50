81963767265ce018f7b3e7a65103f252116bcab1
painkiller2001 <fsvfsvfsva@yandex.by>
2026-04-02 23:08:10 +0300

    generate_diff logic realisation

--- gen_diff/diff/gendiff.py
+++ gen_diff/diff/gendiff.py
@@ -19,3 +19,29 @@ def get_reference_information():
         -f FORMAT, --format FORMAT
                                 set format of output
         ''')
+
+def generate_diff(file1, file2):
+
+    result = {}
+
+    sorted_data_file1 = dict(sorted(file1.items()))
+    sorted_data_file2 = dict(sorted(file2.items()))
+
+    for key, value in sorted_data_file1.items():
+        if key not in sorted_data_file2:
+            result[f'- {key}'] = str(value).lower()
+        if key in sorted_data_file2 and sorted_data_file1[key] == sorted_data_file2[key]:
+            result[f'  {key}'] = str(value).lower()
+        if key in sorted_data_file2 and sorted_data_file1[key] != sorted_data_file2[key]:
+            result[f'- {key}'] = str(value).lower()
+            result[f'+ {key}'] = str(sorted_data_file2[key]).lower()
+
+    for key, value in sorted_data_file2.items():
+        if f'  {key}' in result:
+            continue
+        else:
+            result[f'+ {key}'] = str(value).lower()
+        
+    formatted_result = '\n'.join(f"{key}: {value}" for key, value in result.items())
+
+    return formatted_result
