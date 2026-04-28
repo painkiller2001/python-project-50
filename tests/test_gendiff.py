1fd6b38c4480521b3f4721177f08f0a1305953a9
painkiller2001 <fsvfsvfsva@yandex.by>
2026-04-28 08:50:13 +0300

    linter adusted 4/28

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 from gen_diff.diff.gendiff import generate_diff
-from tests.test_data.expected_results.result_formatters.result_gendiff_json import (
-    get_expected_result_json,
+from tests.test_data.expected_results.result_formatters import (
+    result_gendiff_json,
+    result_gendiff_plain,
+    result_gendiff_stylish,
 )
-from tests.test_data.expected_results.result_formatters.result_gendiff_plain import (
-    get_expected_result_plain,
-)
-from tests.test_data.expected_results.result_formatters.result_gendiff_stylish import (
-    get_expected_result_stylish,
-)
+
+get_expected_result_json = result_gendiff_json.get_expected_result_json
+get_expected_result_plain = result_gendiff_plain.get_expected_result_plain
+get_expected_result_stylish = result_gendiff_stylish.get_expected_result_stylish
 
 TEST_DATA_DIR = Path(__file__).parent / 'test_data'
 
@@ -25,75 +25,75 @@ EXPECTED_STYLISH_BASIC = '''{
 
 def test_generate_diff_stylish_basic_json():
     result = generate_diff(
-        'test_data_file1.json', 
-        'test_data_file2.json', 
-        TEST_DATA_DIR, 
+        'test_data_file1.json',
+        'test_data_file2.json',
+        TEST_DATA_DIR,
         'stylish'
-        )
+    )
     assert result.strip() == EXPECTED_STYLISH_BASIC.strip()
 
 
 def test_generate_diff_stylish_basic_yaml():
     result = generate_diff(
-        'test_data_file1.yml', 
-        'test_data_file2.yml', 
-        TEST_DATA_DIR, 
+        'test_data_file1.yml',
+        'test_data_file2.yml',
+        TEST_DATA_DIR,
         'stylish'
-        )
+    )
     assert result.strip() == EXPECTED_STYLISH_BASIC.strip()
 
 
 def test_generate_diff_stylish_complex_json():
     expected = get_expected_result_stylish().strip()
     result = generate_diff(
-        'test_data_file5.json', 
-        'test_data_file6.json', 
-        TEST_DATA_DIR, 
+        'test_data_file5.json',
+        'test_data_file6.json',
+        TEST_DATA_DIR,
         'stylish'
-        )
+    )
     assert result.strip() == expected
 
 
 def test_generate_diff_stylish_complex_yaml():
     expected = get_expected_result_stylish().strip()
     result = generate_diff(
-        'test_data_file5.yml', 
-        'test_data_file6.yml', 
-        TEST_DATA_DIR, 
+        'test_data_file5.yml',
+        'test_data_file6.yml',
+        TEST_DATA_DIR,
         'stylish'
-        )
+    )
     assert result.strip() == expected
 
 
 def test_generate_diff_plain_complex_json():
     expected = get_expected_result_plain().strip()
     result = generate_diff(
-        'test_data_file5.json', 
-        'test_data_file6.json', 
-        TEST_DATA_DIR, 
+        'test_data_file5.json',
+        'test_data_file6.json',
+        TEST_DATA_DIR,
         'plain'
-        )
+    )
     assert result.strip() == expected
 
 
 def test_generate_diff_plain_complex_yaml():
     expected = get_expected_result_plain().strip()
     result = generate_diff(
-        'test_data_file5.yml', 
-        'test_data_file6.yml', 
-        TEST_DATA_DIR, 
+        'test_data_file5.yml',
+        'test_data_file6.yml',
+        TEST_DATA_DIR,
         'plain'
-        )
+    )
     assert result.strip() == expected
 
 
 def test_generate_diff_json_complex_json():
     result = generate_diff(
-        'test_data_file5.json', 
-        'test_data_file6.json', 
-        TEST_DATA_DIR, 
+        'test_data_file5.json',
+        'test_data_file6.json',
+        TEST_DATA_DIR,
         'json'
-        )
+    )
     expected = get_expected_result_json()
     assert result.strip() == expected.strip()
 
@@ -101,9 +101,9 @@ def test_generate_diff_json_complex_json():
 def test_generate_diff_json_complex_yaml():
     expected = get_expected_result_json().strip()
     result = generate_diff(
-        'test_data_file5.yml', 
-        'test_data_file6.yml', 
-        TEST_DATA_DIR, 
+        'test_data_file5.yml',
+        'test_data_file6.yml',
+        TEST_DATA_DIR,
         'json'
-        )
+    )
     assert result.strip() == expected
